5734ed7752276c3b65ec926c285a3c384a9bb50b
Emiliano Lopez Denegri <emiliano.lopez@ing.austral.edu.ar>
2023-08-18 23:13:53 -0300

    emilianolopez/eve-31-button-typography-and-text-field-components (#2)
    * setup project
    
    * add router dom
    remove material ui
    
    * add button component
    
    * add textfield component
    
    * add typography component
    
    * changes

---
+++ src/components/TextField/index.jsx
@@ -0,0 +1,31 @@
+import Typography from "../Typography"
+import "./styles.css"
+import PropTypes from "prop-types"
+
+export default function TextField({label, name, value, onChange, helperText, placeholder, error}){
+    return (
+        <div className="textfield-container">
+            <Typography variant={"body2"}>{label}</Typography>
+            <input type="text" 
+                placeholder={placeholder} 
+                name={name} 
+                value={value} 
+                onChange={onChange} 
+                className={error ? "input-error" : ""}
+            />
+            {helperText && 
+                <Typography variant={"body3"} className={error ? "error" : ""}>{helperText}</Typography>
+            }
+        </div>
+    )
+}
+
+TextField.propTypes ={
+    label: PropTypes.string,
+    name: PropTypes.string,
+    value: PropTypes.string,
+    onChange: PropTypes.func,
+    helperText: PropTypes.string,
+    placeholder: PropTypes.string,
+    error: PropTypes.bool
+}
