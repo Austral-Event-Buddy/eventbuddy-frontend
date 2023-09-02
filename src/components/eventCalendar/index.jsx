462c3f5153aa7bc08b7a947537e3c2efdc2b60f5
Emi <emiliano.lopez@ing.austral.edu.ar>
2023-09-01 22:11:24 -0300

    add some style add mock days for demo

--- src/components/eventCalendar/index.jsx
+++ src/components/eventCalendar/index.jsx
@@ -1,16 +1,21 @@
-import { useState } from 'react';
 import { DayPicker } from 'react-day-picker';
 import 'react-day-picker/dist/style.css';
 import './styles.css'
 
 export default function EventCalendar() {
-    const [selected, setSelected] = useState([]);
+    const mockDays = [
+        new Date(), 
+        new Date("September 17, 2023"), 
+        new Date("September 30, 2023"), 
+        new Date("August 29, 2023")
+    ];
+
+    //we could also use UseState
 
     return (
         <DayPicker
             mode="multiple"
-            selected={selected}
-            onSelect={setSelected}
+            selected={mockDays}
             showOutsideDays 
         />
     )
