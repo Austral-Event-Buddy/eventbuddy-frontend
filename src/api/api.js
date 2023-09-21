f1794c7610dbcc0c07b178047d0b919f15542df4
Emi <emiliano.lopez@ing.austral.edu.ar>
2023-09-20 23:15:11 -0300

    add integration with backend for calendar events

--- src/api/api.js
+++ src/api/api.js
@@ -20,7 +20,7 @@ export const register = async (body, callback) => {
   callback();
 };
 
-export const home = async () => {
-    const res = await api.get('event');
+export const getEvents = async () => {
+    const res = await api.get('event/getEvents');
     return res.data
 };
