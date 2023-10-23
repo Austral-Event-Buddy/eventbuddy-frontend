80e978df1b8c349a05fa5a94518197e9dd087cf2
Emi <emiliano.lopez@ing.austral.edu.ar>
2023-10-23 20:07:31 -0300

    add missing endpoint

--- src/api/api.js
+++ src/api/api.js
@@ -21,31 +21,36 @@ export const register = async (body, callback) => {
 };
 
 export const getEvents = async () => {
-    const res = await api.get('event');
-    return res.data
+  const res = await api.get('event');
+  return res.data;
 };
 
 export const getEventById = async (id) => {
   const res = await api.get(`event/${id}`);
-  return res.data
+  return res.data;
 };
 
 export const searchEvents = async (search) => {
   const res = await api.get(`event/search`, { params: { search } });
-  return res.data
-}
+  return res.data;
+};
 
 export const answerInvite = async (body) => {
   const res = await api.put(`event/invite/answer`, body);
-  return res.data
-}
+  return res.data;
+};
 
 export const inviteGuest = async (body) => {
   const res = await api.post('event/invite/send', body);
-  return res.data
-}
+  return res.data;
+};
 
 export const searchUsers = async (username) => {
   const res = await api.get(`/user/by_username/${username}`);
+  return res.data;
+};
+
+export const createEvent = async (body) => {
+  const res = await api.post('event', body);
   return res.data;
 };
