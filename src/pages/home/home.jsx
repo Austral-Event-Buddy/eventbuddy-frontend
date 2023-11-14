70d44c7d3f8109fb02082567d0f9967417d16c7a
Emiliano Lopez Denegri <emiliano.lopez@ing.austral.edu.ar>
2023-11-13 21:48:24 -0300

    Emilianolopez/reviews (#41)
    * add rating panel
    
    * add rates component
    
    * fix missing bracket

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -8,6 +8,7 @@ import { useEffect, useState } from "react";
 import { getEvents, searchEvents } from '../../api/api';
 import { useNavigate } from 'react-router-dom';
 import EventModal from "../../components/CreateEventModal";
+import Rates from '../../components/Rates';
 
 export default function Home() {
     const navigate = useNavigate()
@@ -74,7 +75,10 @@ export default function Home() {
                         />
                     ))}
                 </div>
+                <div className='info'>
                 <EventCalendar mode='multiple' events={events} onClick={handleDayClick} />
+                <Rates/>
+                </div>
             </div>
             <div className="button-container">
                 <Button onClick={(e) => {
